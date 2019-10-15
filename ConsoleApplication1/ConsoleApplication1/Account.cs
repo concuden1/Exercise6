858917e30121c3047a8972c7bbcbd77ddaf22300
Lan31 <duylan426@gmail.com>
2019-10-15 15:27:25 +0700

    Exercise6

---
+++ ConsoleApplication1/ConsoleApplication1/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Account
+    {
+        public Account(int amount)
+        {
+
+        }
+        public void Deposit(int amount)
+        {
+
+        }
+
+        public void Withdraw(int amount)
+        {
+
+        }
+
+        public int Balance()
+        {
+            return 0;
+        }
+    }
+}
